84e525eb73bfb03e6d8c360996d987e505c1c2af
unknown <ujjwal060>
2025-08-06 15:34:12 +0530

    add change

--- controllers/reserveController.js
+++ controllers/reserveController.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Reserve = require('../models/reserveModel');
-const Bookform = require('../models/checkoutModel');
+const express = require("express");
+const mongoose = require("mongoose");
+const Reserve = require("../models/reserveModel");
+const Bookform = require("../models/checkoutModel");
 const router = express.Router();
-const Payment = require('../models/PaymentModel');
-const nodemailer = require('nodemailer');
-const User = require('../models/userModel');
+const Payment = require("../models/PaymentModel");
+const nodemailer = require("nodemailer");
+const User = require("../models/userModel");
 const Vehicle = require("../models/vehicleModel");
 const NewVehicle = require("../models/newVehicleModel");
-const bookingModdel=require("../models/checkoutModel");
+const bookingModdel = require("../models/checkoutModel");
 
 // Create a new reservation
 const createReservation = async (req, res) => {
@@ -27,49 +27,53 @@ const createReservation = async (req, res) => {
   }
 };
 
-
 const getAllReservations = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
-    const searchQuery = req.query.search || '';
+    const searchQuery = req.query.search || "";
 
     const skip = (page - 1) * limit;
 
     const vehicleFilter = searchQuery
       ? {
-        $or: [
-          { vname: { $regex: searchQuery, $options: 'i' } },
-          { tagNumber: { $regex: searchQuery, $options: 'i' } },
-        ],
-      }
+          $or: [
+            { vname: { $regex: searchQuery, $options: "i" } },
+            { tagNumber: { $regex: searchQuery, $options: "i" } },
+          ],
+        }
       : {};
 
-    const vehicles = await NewVehicle.find(vehicleFilter).select('_id');
+    const vehicles = await NewVehicle.find(vehicleFilter).select("_id");
     const vehicleIds = vehicles.map((vehicle) => vehicle._id);
 
-    const reservationFilter = searchQuery
-      ? { vehicleId: { $in: vehicleIds } }
-      : {};
+    const reservationFilter = {
+      bookingType: "Reservation",
+      ...(searchQuery && { vehicleId: { $in: vehicleIds } }),
+    };
 
     const reservations = await Reserve.find(reservationFilter)
       .skip(skip)
       .limit(limit)
       .sort({ createdAt: -1 });
 
-   
     const enrichedReservations = await Promise.all(
       reservations.map(async (reservation) => {
         let vehicleDetails = null;
         if (reservation.vehicleId) {
-          vehicleDetails = await NewVehicle.findOne({ _id: reservation.vehicleId })
-            .select('vname tagNumber passenger image model');
+          vehicleDetails = await NewVehicle.findOne({
+            _id: reservation.vehicleId,
+          }).select("vname tagNumber passenger image model");
         }
-        const payment = await Payment.findOne({ reservation: reservation._id }).select('userId');
-        const bookingdata = await bookingModdel.findOne({ reservationId: reservation._id }).select('invoiceNumber');
+        const payment = await Payment.findOne({
+          reservation: reservation._id,
+        }).select("userId");
+        // const bookingdata = await bookingModdel
+        //   .findOne({ reservationId: reservation._id })
+        //   .select("invoiceNumber");
         let userName = null;
         if (payment?.userId) {
-          const user = await User.findById(payment.userId).select('fullName');
+          const user = await User.findById(payment.userId).select("fullName");
           userName = user?.fullName || null;
         }
 
@@ -77,7 +81,7 @@ const getAllReservations = async (req, res) => {
           ...reservation.toObject(),
           vehicleDetails,
           userName,
-          invoiceNumber:bookingdata.invoiceNumber || 'N/A'
+          // invoiceNumber: bookingdata.invoiceNumber || "N/A",
         };
       })
     );
@@ -88,7 +92,7 @@ const getAllReservations = async (req, res) => {
 
     res.json({
       success: true,
-      message: 'All Reservations',
+      message: "All Reservations",
       data: enrichedReservations,
       pagination: {
         totalReservations,
@@ -105,11 +109,7 @@ const getAllReservations = async (req, res) => {
 const getAllReservationsFromPanel = async (req, res) => {
   try {
     const reservations = await Reserve.find({
-      $and: [
-        { reservation: true },
-        { booking: false },
-        { fromAdmin: true },
-      ]
+      $and: [{ reservation: true }, { booking: false }, { fromAdmin: true }],
     });
 
     const populatedReservations = await Promise.all(
@@ -117,7 +117,7 @@ const getAllReservationsFromPanel = async (req, res) => {
         if (reservation.vehicleId) {
           const vehicle = await NewVehicle.findOne(
             { _id: reservation.vehicleId },
-            'vname passenger image tagNumber model'
+            "vname passenger image tagNumber model"
           );
           return {
             ...reservation.toObject(),
@@ -137,7 +137,6 @@ const getAllReservationsFromPanel = async (req, res) => {
   }
 };
 
-
 //accept
 const acceptReservation = async (req, res) => {
   try {
@@ -151,7 +150,9 @@ const acceptReservation = async (req, res) => {
     await reservation.save();
     const payment = await Payment.findOne({ reservation: id });
     if (!payment) {
-      return res.status(404).json({ message: "Payment record not found for this reservation." });
+      return res
+        .status(404)
+        .json({ message: "Payment record not found for this reservation." });
     }
 
     const user = await User.findById(payment.userId);
@@ -159,18 +160,18 @@ const acceptReservation = async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
+        pass: process.env.EMAIL_PASS,
+      },
     });
 
     const mailOptions = {
       from: process.env.EMAIL_USER,
       to: user.email,
-      subject: 'Reservation Accepted',
-      text: `Your reservation with ID ${id} has been accepted. Kindly continue with your booking. Thank you!`
+      subject: "Reservation Accepted",
+      text: `Your reservation with ID ${id} has been accepted. Kindly continue with your booking. Thank you!`,
     };
 
     await transporter.sendMail(mailOptions);
@@ -211,7 +212,6 @@ const updateReservation = async (req, res) => {
   }
 };
 
-
 const getReservationListingByDriverID = async (req, res) => {
   try {
     const driverId = req.params.driverId;
@@ -219,29 +219,31 @@ const getReservationListingByDriverID = async (req, res) => {
     // Find all reservations associated with the provided driver ID
     const reservations = await Bookform.find({ driver: driverId })
       .populate({
-        path: 'reservationId',
+        path: "reservationId",
         populate: {
-          path: 'userId', // Populate user details in reservationId
-          model: 'User',
-          select: 'fullName email phoneNumber image', // Include image field
-        }
+          path: "userId", // Populate user details in reservationId
+          model: "User",
+          select: "fullName email phoneNumber image", // Include image field
+        },
       })
-      .populate('driver')
-      .populate('paymentId')
+      .populate("driver")
+      .populate("paymentId")
       .exec();
 
     if (reservations.length === 0) {
-      return res.status(404).json({ message: 'No reservations found for this driver' });
+      return res
+        .status(404)
+        .json({ message: "No reservations found for this driver" });
     }
 
     return res.status(200).json({
       success: true,
       message: "Reservations Listing",
-      data: reservations
+      data: reservations,
     });
   } catch (error) {
-    console.error('Error fetching reservations:', error);
-    res.status(500).json({ message: 'Server error' });
+    console.error("Error fetching reservations:", error);
+    res.status(500).json({ message: "Server error" });
   }
 };
 
@@ -266,6 +268,5 @@ module.exports = {
   updateReservation,
   deleteReservation,
   getReservationListingByDriverID,
-  getAllReservationsFromPanel
+  getAllReservationsFromPanel,
 };
-
